89c640dff016e01b13d48c2f16279353c506795f
Paul Reyes <72253352+PSR-04@users.noreply.github.com>
2023-06-16 02:00:09 +0800

    Add files via upload

---
+++ FindArray.java
@@ -0,0 +1,19 @@
+// Finding the Maximum Value in an Array
+
+public class FindArray {
+  public static void main(String[] args) {
+  int[] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 8};
+  int max = numbers[0];
+  
+  /* Process here the max value will be save to max variable use loop */
+
+  for(int i= 0; i < numbers.length; i++) {
+    
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+
+  System.out.println("The maximum value in the array is: " + max);
+  }
+}
